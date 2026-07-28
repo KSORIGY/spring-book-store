3bb40965c7152d4f8b3ab08e7ce5d4d58b8571a7
Denny Hirniak <denforever777@gmail.com>
2026-07-28 18:44:48 +0300

    Initial commit: Spring Boot project skeleton

---
+++ src/test/java/my/academy/springbookstore/SpringBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package my.academy.springbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
