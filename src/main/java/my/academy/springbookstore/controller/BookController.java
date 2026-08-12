27e38bc64e3250a1a85385f746f72a3fe629eb9a
Denny Hirniak <denforever777@gmail.com>
2026-08-12 13:36:35 +0300

    add context path to app.properties, add status code CREATED for posting book

--- src/main/java/my/academy/springbookstore/controller/BookController.java
+++ src/main/java/my/academy/springbookstore/controller/BookController.java
@@ -5,15 +5,17 @@ import lombok.RequiredArgsConstructor;
 import my.academy.springbookstore.dto.BookDto;
 import my.academy.springbookstore.dto.CreateBookRequestDto;
 import my.academy.springbookstore.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
@@ -29,6 +31,7 @@ public class BookController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDto save(@RequestBody CreateBookRequestDto createBookRequestDto) {
         return bookService.save(createBookRequestDto);
     }
