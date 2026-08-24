3eb5febf8bbdfc179b0a910a58703a73be6a3390
Denny Hirniak <denforever777@gmail.com>
2026-08-24 19:13:25 +0300

    add validation and global exceptiom handler

---
+++ src/main/java/my/academy/springbookstore/exception/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package my.academy.springbookstore.exception;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+                .map(this::getErrorMessage)
+                .toList();
+
+        return buildErrorResponse(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    private String getErrorMessage(ObjectError e) {
+        if (e instanceof FieldError fieldError) {
+            return fieldError.getField() + " - " + e.getDefaultMessage();
+        }
+
+        return e.getDefaultMessage();
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Object errors, HttpStatus httpStatus) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now().toString());
+        body.put("status", httpStatus);
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, httpStatus);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException exception) {
+        return buildErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
