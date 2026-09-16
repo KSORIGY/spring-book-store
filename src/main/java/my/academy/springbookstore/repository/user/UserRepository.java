60ac081a0e73a5c734ae540cf20fbedc366cb4a9
Denny Hirniak <denforever777@gmail.com>
2026-09-16 18:07:51 +0300

    codestyle fixes

--- src/main/java/my/academy/springbookstore/repository/user/UserRepository.java
+++ src/main/java/my/academy/springbookstore/repository/user/UserRepository.java
@@ -1,6 +1,5 @@
 package my.academy.springbookstore.repository.user;
 
-import java.util.Optional;
 import my.academy.springbookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
