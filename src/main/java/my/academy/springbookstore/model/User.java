81b211f2d66d7081c1fec4930518f68a32a24236
Denny Hirniak <denforever777@gmail.com>
2026-09-23 01:07:42 +0300

    use correct method to find user by email with roles, codestyle fixes

--- src/main/java/my/academy/springbookstore/model/User.java
+++ src/main/java/my/academy/springbookstore/model/User.java
@@ -2,7 +2,6 @@ package my.academy.springbookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,7 +17,6 @@ import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
