2f3bd1e1ef6b9dcb6949d5b0b304efadf3958876
Denny Hirniak <denforever777@gmail.com>
2026-09-22 20:02:25 +0300

    add second part of Spring security: - implment SecurityConfig logic - add Roles - add checks in controller - add encoding to UserServiceImpl

--- src/main/java/my/academy/springbookstore/model/User.java
+++ src/main/java/my/academy/springbookstore/model/User.java
@@ -2,6 +2,7 @@ package my.academy.springbookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,11 +10,15 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.Collection;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
@@ -21,7 +26,7 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("is_deleted = false")
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -36,7 +41,7 @@ public class User {
     private String shippingAddress;
     @Column(nullable = false, columnDefinition = "TINYINT")
     private boolean isDeleted = false;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
@@ -44,4 +49,35 @@ public class User {
     )
     private Set<Role> roles;
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !isDeleted;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .toList();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
--- src/main/java/my/academy/springbookstore/repository/role/RoleRepository.java
+++ src/main/java/my/academy/springbookstore/repository/role/RoleRepository.java
@@ -1,7 +1,10 @@
 package my.academy.springbookstore.repository.role;
 
+import java.util.Optional;
 import my.academy.springbookstore.model.Role;
+import my.academy.springbookstore.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName name);
 }
